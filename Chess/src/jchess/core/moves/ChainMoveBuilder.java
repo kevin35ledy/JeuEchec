397e3ff6eaba9a3d9ae0bc2805b45a0ec065d1b5
phcollin <pierrehenri.collin@gmail.com>
2016-05-03 10:26:52 +0200

    concrete move builder

--- Chess/src/jchess/core/moves/ChainMoveBuilder.java
+++ Chess/src/jchess/core/moves/ChainMoveBuilder.java
@@ -2,4 +2,28 @@ package jchess.core.moves;
 
 public class ChainMoveBuilder extends MoveBuilder {
 
+	@Override
+	public MoveBuilder xFrom(int xF) {
+		move.setXFrom(xF);	
+		return this;
+	}
+
+	@Override
+	public MoveBuilder yFrom(int yF) {
+		move.setXFrom(yF);	
+		return this;
+	}
+
+	@Override
+	public MoveBuilder xTo(int xTo) {
+		move.setXFrom(xTo);	
+		return this;
+	}
+
+	@Override
+	public MoveBuilder yTo(int yTo) {
+		move.setXFrom(yTo);	
+		return this;	
+	}
+
 }
